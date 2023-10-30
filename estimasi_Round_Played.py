3dd08b24662f0f6eb807aa0b0d149cc5285aed24
lennn19 <149348087+lennn19@users.noreply.github.com>
2023-10-30 11:37:18 +0700

    Add files via upload

--- estimasi_Round_Played.py
+++ estimasi_Round_Played.py
@@ -5,10 +5,10 @@ model = pickle.load(open('estimasi_Round_Played.sav', 'rb'))
 
 st.title('Estimasi Round Played')
 
-Kill = st.number_input('Masukan Total Kill')
-Death = st.number_input('Masukan Total Death Played')
-Rounds_Win = st.number_input('Masukan Total Kemenangan Team')
-Rounds_Lose = st.number_input('Masukan Total Kekalahan Team')
+Kill = st.number_input('Masukan Total Kill', min_value=1, step=1)
+Death = st.number_input('Masukan Total Death Played', min_value=1, step=1)
+Rounds_Win = st.number_input('Masukan Total Kemenangan Team', min_value=1, step=1)
+Rounds_Lose = st.number_input('Masukan Total Kekalahan Team', min_value=1, step=1)
 KD = st.number_input('Masukan Total Kill Death Played')
 HS = st.number_input('Masukan Ratio Headshot')
 
@@ -16,6 +16,6 @@ predict = ''
 
 if st.button('Estimasi Round Played'):
     predict = model.predict(
-        [[ 'Kill', 'Death', 'Rounds Win', 'Rounds Lose', 'K/D', 'HS %']]
+        [[ Kill, Death, Rounds_Win, Rounds_Lose, KD, HS]]
     )
     st.write ('Estimasi Round Played : ', predict)
